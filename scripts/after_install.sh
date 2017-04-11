adf3958a1f1ff848730b74cd25e6a110ce99c029
LMacchi <lau.macchi@gmail.com>
2017-04-10 22:14:07 -0500

    Change puppet job path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Run commands after everything is up
 # Run puppet in all nodes
-sudo /opt/puppetlabs/puppet/bin/puppet job run --no-enforce-environment --query 'nodes {deactivated is null and expired is null}'
+sudo /opt/puppetlabs/client-tools/bin/puppet-job run --no-enforce-environment --query 'nodes {deactivated is null and expired is null}'
 # Sign CMs certificates
 sudo /opt/puppetlabs/puppet/bin/puppet cert sign --allow-dns-alt-names --all
 # Run puppet in the CMs
-sudo /opt/puppetlabs/puppet/bin/puppet job run --no-enforce-environment --query 'nodes { certname ~ \"cm\" }'
+sudo /opt/puppetlabs/client-tools/bin/puppet-job run --no-enforce-environment --query 'nodes { certname ~ \"cm\" }'
 # Run puppet in the master
 sudo /opt/puppetlabs/puppet/bin/puppet agent -t
 # Run puppet globally
-sudo /opt/puppetlabs/puppet/bin/puppet job run --no-enforce-environment --query 'nodes {deactivated is null and expired is null}'
+sudo /opt/puppetlabs/client-tools/bin/puppet-job run --no-enforce-environment --query 'nodes {deactivated is null and expired is null}'
