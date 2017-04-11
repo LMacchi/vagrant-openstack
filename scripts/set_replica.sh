ba3761e6519c8858eb87fa363fe1337406d07b4d
LMacchi <lau.macchi@gmail.com>
2017-04-10 22:16:15 -0500

    Change puppet job path again

--- scripts/set_replica.sh
+++ scripts/set_replica.sh
@@ -3,4 +3,4 @@ sudo /opt/puppetlabs/puppet/bin/puppet infrastructure provision replica replica.
 # PuppetDB needs to sync, give it some time
 wait 20 &&
 sudo /opt/puppetlabs/puppet/bin/puppet infrastructure enable -y --topology mono-with-compile --infra-agent-server-urls lb.lmacchi.vm:8140 replica replica.lmacchi.vm &&
-sudo /opt/puppetlabs/puppet/bin/puppet job run --no-enforce-environment --query 'nodes {deactivated is null and expired is null}'
+sudo /opt/puppetlabs/client-tools/bin/puppet-job run --no-enforce-environment --query 'nodes {deactivated is null and expired is null}'
