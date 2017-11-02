03256ac865e49ca0b218ad551346aa6fe18d2af4
LMacchi <lau.macchi@gmail.com>
2017-11-02 17:03:36 -0500

    Change set_replica scripts

---
+++ scripts/set_replica_mono.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# If puppet is running, wait
+sudo /vagrant/scripts/wait_for_puppet.sh &&
+# Proceed. Provision replica
+sudo /opt/puppetlabs/puppet/bin/puppet infrastructure provision replica replica.lmacchi.vm  &&
+# PuppetDB needs to sync, give it some time
+sudo /vagrant/scripts/wait_for_puppetdb_sync.sh &&
+# If puppet is running, wait
+sudo /vagrant/scripts/wait_for_puppet.sh &&
+# Proceed. Enable replica
+sudo /opt/puppetlabs/puppet/bin/puppet infrastructure enable -y --topology mono \
+  --agent-server-urls lb.lmacchi.vm:8140,replica.lmacchi.vm:8140 \
+  --infra-agent-server-urls master.lmacchi.vm:8140,replica.lmacchi.vm:8140 \
+  --pcp-brokers master.lmacchi.vm:4433,replica.lmacchi.vm:4433 \
+  --infra-pcp-brokers master.lmacchi.vm:4433,replica.lmacchi.vm:4433 \
+  --puppetdb-termini master.lmacchi.vm:8081,replica.lmacchi.vm:8081 \
+  --classifier-termini master.lmacchi.vm:4433,replica.lmacchi.vm:4433 \
+  replica replica.lmacchi.vm &&
+# Run puppet in all the infrastructure
+sudo /opt/puppetlabs/puppet/bin/puppet job run --no-enforce-environment --query 'nodes {deactivated is null and expired is null}'
